5fa0973e8fd9c34d235f863b5a94e656fe40a0c3
wolfsschanze911 <anjekur72@gmail.com>
2026-07-07 18:28:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import threading
 
 from dashboard import render_dashboard
 from scan_engine import start_scan
-from state import scan_state
 
 
 # ==========================================
@@ -24,24 +23,61 @@ st.title("WOLFSSCHANZE HW PROJECT")
 
 
 # ==========================================
+# SESSION STATE
+# ==========================================
+
+if "scan_running" not in st.session_state:
+
+    st.session_state.scan_running = False
+
+
+
+# ==========================================
+# CALLBACK SCAN
+# ==========================================
+
+def run_scan():
+
+    start_scan()
+
+    st.session_state.scan_running = False
+
+
+
+# ==========================================
 # BUTTON
 # ==========================================
 
-if scan_state["running"]:
+if st.session_state.scan_running:
+
     button_text = "⏳ SCANNING..."
+
 else:
+
     button_text = "🚀 SCAN SEMUA TOKO"
+
+
+
 if st.button(
     button_text,
     use_container_width=True,
-    disabled=scan_state["running"]
+    disabled=st.session_state.scan_running
 ):
+
+    st.session_state.scan_running = True
+
+
     thread = threading.Thread(
-        target=start_scan,
+        target=run_scan,
         daemon=True
     )
-    
+
     thread.start()
+
+
+    st.rerun()
+
+
 
 # ==========================================
 # DASHBOARD
