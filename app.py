a420ff085e2b52299ed2faa313b527398ed7e398
wolfsschanze911 <anjekur72@gmail.com>
2026-07-07 13:50:31 +0700

    Refactor app.py to improve structure and readability

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import streamlit as st
 from google.oauth2.service_account import Credentials
 
 def connect_to_sheets(): 
-    st.write(type(st.secrets["gcp_service_account"]))
     try:
         creds_dict = dict(st.secrets["gcp_service_account"])
 
