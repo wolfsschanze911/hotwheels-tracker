a86561ed714a916533a81f155b70d013aa80340f
wolfsschanze911 <anjekur72@gmail.com>
2026-07-07 17:56:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import time
 from dashboard import render_dashboard
 from scan_engine import start_scan
 
-
 # ==========================================
 # PAGE CONFIG
 # ==========================================
@@ -15,83 +14,67 @@ st.set_page_config(
     layout="centered"
 )
 
-
 # ==========================================
 # TITLE
 # ==========================================
 
 st.title("🚗 Hot Wheels Tracker")
 
-
 # ==========================================
 # SESSION STATE
 # ==========================================
 
 if "scan_running" not in st.session_state:
-
     st.session_state.scan_running = False
 
-
 if "scan_finished" not in st.session_state:
-
     st.session_state.scan_finished = False
 
 
-
 # ==========================================
-# DASHBOARD AREA
+# HANDLE SCAN FINISH
 # ==========================================
 
-dashboard_placeholder = st.empty()
-
+if st.session_state.scan_finished:
+    st.session_state.scan_running = False
+    st.session_state.scan_finished = False
 
 
 # ==========================================
 # BUTTON
 # ==========================================
 
-if "scan_running" not in st.session_state:
-    st.session_state.scan_running = False
-
-if "scan_done" not in st.session_state:
-    st.session_state.scan_done = False
-
 if st.session_state.scan_running:
     button_text = "⏳ SCANNING..."
-
 else:
     button_text = "🚀 SCAN SEMUA TOKO"
+
 if st.button(
     button_text,
     use_container_width=True,
     disabled=st.session_state.scan_running
 ):
     st.session_state.scan_running = True
-   
+
+
     def run_scan():
         start_scan()
         st.session_state.scan_finished = True
-        st.session_state.scan_done = True
+
     thread = threading.Thread(
-        target=run_scan
+        target=run_scan,
+        daemon=True
     )
     thread.start()
     st.rerun()
 
-if st.session_state.scan_done:
-    st.session_state.scan_done = False
-    st.rerun()
-
 # ==========================================
-# LIVE UPDATE
+# DASHBOARD LIVE
 # ==========================================
 
+dashboard_placeholder = st.empty()
 while True:
-
-
     with dashboard_placeholder.container():
-
         render_dashboard()
-
-
     time.sleep(1)
+    st.rerun()
