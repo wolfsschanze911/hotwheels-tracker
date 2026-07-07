54611e22ea245b107086b86d4c3be0154c52b062
wolfsschanze911 <anjekur72@gmail.com>
2026-07-07 22:11:49 +0700

    Update search_engine.py

--- search_engine.py
+++ search_engine.py
@@ -1,32 +1,44 @@
+from collections import defaultdict
 from state import scan_results
 
+
 def search_product(keyword):
-  
-    results = []
-    keyword = keyword.lower().strip()
-    if not keyword:
-       
-      return results
+
+    keyword = keyword.strip().lower()
+
+    if keyword == "":
+        return []
+
+    grouped = defaultdict(list)
 
     for item in scan_results:
 
-        produk = item.get(
-            "produk",
-            ""
-        ).lower()
+        nama = item["produk"].lower()
 
-        toko = item.get(
-            "toko",
-            ""
-        ).lower()
+        if keyword in nama:
 
+            grouped[item["produk"]].append(item)
 
-        if (
-            keyword in produk
-            or keyword in toko
-        ):
+    results = []
 
-            results.append(item)
+    for produk, toko_list in grouped.items():
 
+        toko_list.sort(
+            key=lambda x: x["stok"],
+            reverse=True
+        )
+
+        results.append({
+
+            "produk": produk,
+
+            "toko": toko_list
+
+        })
+
+    results.sort(
+        key=lambda x: len(x["toko"]),
+        reverse=True
+    )
 
     return results
