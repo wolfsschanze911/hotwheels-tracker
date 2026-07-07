b23e8c4b518551b796d25f1d9527286d1101e53c
wolfsschanze911 <anjekur72@gmail.com>
2026-07-07 16:37:49 +0700

    Update ui.py

--- ui.py
+++ ui.py
@@ -1,43 +1,41 @@
 import streamlit as st
-from datetime import datetime
 
 
 def dashboard(
     total_toko,
+    scanned_toko,
     total_produk,
     total_baru,
     total_naik,
     total_turun,
-    status="⚪ Belum Scan",
+    progress,
+    status,
+    last_scan,
 ):
-
-    now = datetime.now().strftime("%d %b %Y • %H:%M")
 
     with st.container(border=True):
 
         st.markdown("### 🚗 Hot Wheels Tracker")
 
-        st.caption(status)
-        st.caption(f"🕒 {now}")
+        st.write(status)
+
+        st.caption(f"🕒 {last_scan}")
+
+        st.write("")
 
         st.markdown(
             f"""
-            <div style="
-                font-size:18px;
-                font-weight:600;
-                text-align:center;
-                padding-top:8px;
-            ">
-                🏪 {total_toko}
-                &nbsp;&nbsp;&nbsp;
-                🚗 {total_produk}
-                &nbsp;&nbsp;&nbsp;
-                🆕 {total_baru}
-                &nbsp;&nbsp;&nbsp;
-                🟢 {total_naik}
-                &nbsp;&nbsp;&nbsp;
-                🔴 {total_turun}
-            </div>
+            **🏪 {scanned_toko}/{total_toko}**
+            &nbsp;&nbsp;&nbsp;
+            **🚗 {total_produk}**
+            &nbsp;&nbsp;&nbsp;
+            **🆕 {total_baru}**
+            &nbsp;&nbsp;&nbsp;
+            **🟢 {total_naik}**
+            &nbsp;&nbsp;&nbsp;
+            **🔴 {total_turun}**
             """,
             unsafe_allow_html=True,
         )
+
+        st.progress(progress)
