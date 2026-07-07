ab675ee73d49e4a2a2a5ba42ca4b3c5d85a08b14
wolfsschanze911 <anjekur72@gmail.com>
2026-07-07 16:31:20 +0700

    Update ui.py

--- ui.py
+++ ui.py
@@ -11,62 +11,32 @@ def dashboard(
     status="⚪ Belum Scan",
 ):
 
-    tanggal = datetime.now().strftime("%d %b %Y")
-    jam = datetime.now().strftime("%H:%M")
+    now = datetime.now().strftime("%d %b %Y • %H:%M")
 
     with st.container(border=True):
 
-        st.markdown("#### 🚗 Hot Wheels Tracker")
+        st.markdown("### 🚗 Hot Wheels Tracker")
 
-        st.write(status)
+        st.caption(status)
+        st.caption(f"🕒 {now}")
 
-        st.caption(f"{tanggal}")
-        st.caption(f"{jam}")
-
-        st.divider()
-
-        col1, col2, col3, col4, col5 = st.columns(5)
-
-        col1.markdown(
+        st.markdown(
             f"""
-            <div style="text-align:center">
-            🏪<br><b>{total_toko}</b>
-            </div>
-            """,
-            unsafe_allow_html=True,
-        )
-
-        col2.markdown(
-            f"""
-            <div style="text-align:center">
-            🚗<br><b>{total_produk}</b>
-            </div>
-            """,
-            unsafe_allow_html=True,
-        )
-
-        col3.markdown(
-            f"""
-            <div style="text-align:center">
-            🆕<br><b>{total_baru}</b>
-            </div>
-            """,
-            unsafe_allow_html=True,
-        )
-
-        col4.markdown(
-            f"""
-            <div style="text-align:center">
-            🟢<br><b>{total_naik}</b>
-            </div>
-            """,
-            unsafe_allow_html=True,
-        )
-
-        col5.markdown(
-            f"""
-            <div style="text-align:center">
-            🔴<br><b>{total_turun}</b>
+            <div style="
+                font-size:18px;
+                font-weight:600;
+                text-align:center;
+                padding-top:8px;
+            ">
+                🏪 {total_toko}
+                &nbsp;&nbsp;&nbsp;
+                🚗 {total_produk}
+                &nbsp;&nbsp;&nbsp;
+                🆕 {total_baru}
+                &nbsp;&nbsp;&nbsp;
+                🟢 {total_naik}
+                &nbsp;&nbsp;&nbsp;
+                🔴 {total_turun}
             </div>
             """,
             unsafe_allow_html=True,
