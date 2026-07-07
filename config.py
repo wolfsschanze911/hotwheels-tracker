ed3c6eb0dd5268550ec53751000073278ffce0f0
wolfsschanze911 <anjekur72@gmail.com>
2026-07-07 14:08:59 +0700

    Add configuration for HotWheelsDB and search URL

---
+++ config.py
@@ -0,0 +1,14 @@
+
+
+SPREADSHEET_NAME = "HotWheelsDB"
+WORKSHEET_NAME = "Sheet1"
+
+URL_PENCARIAN = "https://webcommerce-gw.alfagift.id/v2/products/searches?keyword=hot-wheels&start=0&limit=60"
+
+HEADERS = {
+    ...
+}
+
+DAFTAR_TOKO = [
+    ...
+]
